e7ea8440ec303e3f2233bb9746959cda45edad7f
Dace <dace_bobovica@inbox.lv>
2020-01-18 14:08:21 +0200

    even number done

--- src/main/java/jtm/tasks/fundamentals/EvenNumbers.java
+++ src/main/java/jtm/tasks/fundamentals/EvenNumbers.java
@@ -13,6 +13,24 @@ public class EvenNumbers {
             divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
          */
     public static int[] divisibleBy(int[] numbers, int divider) {
-        return null;
-    }
-}
+    	System.out.println(Arrays.toString(numbers));
+    	System.out.println(divider);
+    	
+    	int [] newMassive= new int [numbers.length];
+    	int j=0;
+    	for (int i =0; i<numbers.length; i++){
+    		if (numbers[i] % divider==0){
+    			newMassive[j]=numbers[i];
+    			j++;
+    		}
+    		
+    	}
+    	int k = j--;
+    	int[] massiveDivider = new int [k];
+    	for (int s =0; s<k; s++){
+    		massiveDivider[s]= newMassive[s];
+    	}
+    	System.out.println(Arrays.toString(newMassive));
+    	System.out.println(Arrays.toString(massiveDivider));
+        return massiveDivider;
+    }}
